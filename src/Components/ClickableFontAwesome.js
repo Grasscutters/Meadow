4557c0eafaeddec9842c02621aa8c5c1ae45a74c
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-04-18 08:51:08 +0530

    Init rewrite

--- src/Components/ClickableFontAwesome.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-export default function ClickableFontAwesome(props) {
-  return (
-    <p><a href={props.href}><FontAwesomeIcon icon={props.icon} /></a></p>
-  )
-}
--- src/Components/Copyright.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Copyright() {
-    return (
-        <div id="copyright-notice">
-            <p> &copy; Grasscutter Team 2022 </p>
-        </div>
-    )
-}
