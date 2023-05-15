6603b14d043751c705a716682cb79882b21a71d0
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-05-15 08:20:25 +0530

    Placeholder page

---
+++ src/ui/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="Home">
+                <p>Home</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
