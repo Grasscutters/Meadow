ba58e08a5c1671097549ea1391b25993ae7d6016
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-05-17 06:34:14 +0530

    StyledHeading component

---
+++ src/ui/components/common/StyledHeading.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface IProps {
+    text: string;
+    topColor?: string;
+    bottomColor?: string;
+
+    className?: string;
+}
+
+class StyledHeading extends React.Component<IProps, never> {
+    constructor(props: IProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={"StyledHeading"}>
+                <div className={"StyledHeading_Top"} style={{ background: this.props.topColor }} />
+                <h1 className={"StyledHeading_Text"}>{this.props.text}</h1>
+                <div className={"StyledHeading_Bottom"} style={{ background: this.props.bottomColor }} />
+            </div>
+        );
+    }
+}
+
+export default StyledHeading;
