fab9aee24fad456d13b91b8dc3049ff1ffde2cff
Scald <104459145+Arikatsu@users.noreply.github.com>
2023-05-23 14:27:35 +0530

    Router component

---
+++ src/ui/components/common/Router.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Router = (Children: React.ComponentClass<any>) => {
+    return(props: any) => {
+        const match  = { params: useParams() };
+        return <Children {...props} match={match}/>
+    }
+}
+
+export default Router;
